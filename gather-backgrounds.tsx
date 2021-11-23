625c73a6ca1a644633b85a492bb0f87a87ea37fa
nmrauh <nmrauh@gmail.com>
2021-11-22 16:28:05 -0800

    testing two adjacent activity rooms

---
+++ gather-backgrounds.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="gather-backgrounds" tilewidth="1536" tileheight="1024" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="896" height="1024" source="activity-north.png"/>
+ </tile>
+ <tile id="1">
+  <image width="896" height="1024" source="activity-south.png"/>
+ </tile>
+ <tile id="2">
+  <image width="1536" height="1024" source="festival_center.png"/>
+ </tile>
+</tileset>
