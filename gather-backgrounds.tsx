789dd41d9dbaf9c55f153ed29297080b0fab6b23
nmrauh <nmrauh@gmail.com>
2021-11-23 08:05:38 -0800

    more background stuff

--- gather-backgrounds.tsx
+++ gather-backgrounds.tsx
@@ -2,12 +2,12 @@
 <tileset version="1.5" tiledversion="1.7.2" name="gather-backgrounds" tilewidth="1536" tileheight="1024" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="896" height="1024" source="activity-north.png"/>
+  <image width="896" height="1024" source="../activity-north.png"/>
  </tile>
  <tile id="1">
-  <image width="896" height="1024" source="activity-south.png"/>
+  <image width="896" height="1024" source="../activity-south.png"/>
  </tile>
  <tile id="2">
-  <image width="1536" height="1024" source="festival_center.png"/>
+  <image width="1536" height="1024" source="../festival_center.png"/>
  </tile>
 </tileset>
